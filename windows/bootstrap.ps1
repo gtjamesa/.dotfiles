631ec5afee454150e9c9cb8e245dc01d064213e4
James Austen <aus.james@gmail.com>
2023-07-24 09:43:37 +0100

    yes to git

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -7,7 +7,7 @@ Function Install-Deps {
     Set-ExecutionPolicy AllSigned -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
     # Install git
-    choco install git
+    choco install -y git
 }
 
 Function Clone-Repo {
